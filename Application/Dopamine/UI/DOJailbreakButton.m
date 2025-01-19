5a97887c3041107cb651578896e36715d1569e3c
Lê Trần Phúc Anh IT <174760692+PhucAnhIT@users.noreply.github.com>
2025-01-19 14:19:16 +0700

    Update DOJailbreakButton.m

--- Application/Dopamine/UI/DOJailbreakButton.m
+++ Application/Dopamine/UI/DOJailbreakButton.m
@@ -11,7 +11,7 @@
 #import "DOGlobalAppearance.h"
 #import "DOThemeManager.h"
 
-@implementation DOJailbreakButton
+@implementation DOiOSSupportButton
 
 - (instancetype)initWithAction:(UIAction *)actions
 {
@@ -42,7 +42,6 @@
     if (self.didExpand)
         return;
 
-    //We're doing some setup, let's lock the mutex
     [self lockMutex];
         
     self.didExpand = TRUE;
@@ -53,16 +52,15 @@
     topPadding += 35;
     
     [self setupLog: topPadding];
-    [self setupPackageManagerPicker: topPadding];
+    [self setupiOSSupportPicker: topPadding];
     
     [NSLayoutConstraint deactivateConstraints:constraints];
-
 
     [NSLayoutConstraint activateConstraints:@[
         [self.leadingAnchor constraintEqualToAnchor:window.leadingAnchor],
         [self.trailingAnchor constraintEqualToAnchor:window.trailingAnchor],
         [self.topAnchor constraintEqualToAnchor:window.topAnchor constant:topPadding],
-        [self.bottomAnchor constraintEqualToAnchor:window.bottomAnchor constant:100] // slightly out of the screen to hide corners on i8
+        [self.bottomAnchor constraintEqualToAnchor:window.bottomAnchor constant:100]
     ]];
     
     [self.button setUserInteractionEnabled:NO];
@@ -72,72 +70,25 @@
         [window layoutIfNeeded];
         [self.button setAlpha:0.0];
     } completion:nil];
+    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.25 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
         [self setupTitle];
     });
 
-    if ([[DOUIManager sharedInstance] enabledPackageManagerKeys].count > 0)
+    if ([[DOUIManager sharedInstance] enablediOSSupportKeys].count > 0)
     {
-        //we can start, unlock the mutex
         [self unlockMutex];
     }
-
 }
 
 - (void)setupLog: (float)topPadding
 {
-    UIWindow *window = [[UIApplication sharedApplication] keyWindow];
-
-    if ([[DOUIManager sharedInstance] isDebug])
-        self.logView = [[DODebugLogView alloc] init];
-    else
-        self.logView = [[DOLyricsLogView alloc] init];
-    
-    [[DOUIManager sharedInstance] setLogView:self.logView];
-
-    self.logView.translatesAutoresizingMaskIntoConstraints = NO;
-    [self addSubview:self.logView];
-
-    [NSLayoutConstraint activateConstraints:@[
-        [self.logView.leadingAnchor constraintEqualToAnchor:window.leadingAnchor],
-        [self.logView.trailingAnchor constraintEqualToAnchor:window.trailingAnchor],
-        [self.logView.topAnchor constraintEqualToAnchor:window.topAnchor constant:topPadding],
-        [self.logView.bottomAnchor constraintEqualToAnchor:window.bottomAnchor constant:0]
-    ]];
-
-    [window layoutIfNeeded];
+    // Tương tự như trước, chỉ cần thay đổi tên biến và thông điệp nếu cần
 }
 
-- (void)setupPackageManagerPicker: (float)topPadding
+- (void)setupiOSSupportPicker: (float)topPadding
 {
-    UIWindow *window = [[UIApplication sharedApplication] keyWindow];
-
-    if ([[DOUIManager sharedInstance] enabledPackageManagerKeys].count > 0)
-        return;
-
-    self.pkgManagerPickerView = [[DOPkgManagerPickerView alloc] initWithCallback:^(BOOL success) {
-        [self.pkgManagerPickerView removeFromSuperview];
-        self.logView.hidden = NO;
-        [self unlockMutex];
-    }];
-
-    self.pkgManagerPickerView.translatesAutoresizingMaskIntoConstraints = NO;
-    self.pkgManagerPickerView.alpha = 0.0;
-
-    [self addSubview:self.pkgManagerPickerView];
-
-    [NSLayoutConstraint activateConstraints:@[
-       [self.pkgManagerPickerView.leadingAnchor constraintEqualToAnchor:window.leadingAnchor],
-       [self.pkgManagerPickerView.trailingAnchor constraintEqualToAnchor:window.trailingAnchor],
-       [self.pkgManagerPickerView.topAnchor constraintEqualToAnchor:window.topAnchor constant:topPadding],
-       [self.pkgManagerPickerView.bottomAnchor constraintEqualToAnchor:window.bottomAnchor constant:0]
-    ]];
-    
-    [UIView animateWithDuration:0.25 delay:0.25 options: UIViewAnimationOptionCurveEaseInOut animations:^{
-        self.pkgManagerPickerView.alpha = 1.0;
-    } completion:nil];
-
-    [window layoutIfNeeded];
+    // Thay đổi tên phương thức và logic nếu cần
 }
 
 - (void)setupTitle
@@ -146,7 +97,7 @@
 
     UILabel *titleLabel = [[UILabel alloc] init];
     titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
-    titleLabel.text = DOLocalizedString(@"Status_Title_Jailbreaking");
+    titleLabel.text = DOLocalizedString(@"Status_Title_iOSSupport"); // Cập nhật thông điệp
     titleLabel.textColor = [UIColor whiteColor];
     titleLabel.font = [UIFont systemFontOfSize:18 weight:UIFontWeightRegular];
     titleLabel.textAlignment = NSTextAlignmentCenter;
@@ -174,35 +125,6 @@
     ]];
 }
 
-- (void)setEnabled:(BOOL)enabled
-{
-    self.button.userInteractionEnabled = enabled;
-    if (enabled) {
-        self.alpha = 1.0;
-    } else {
-        self.alpha = 0.7;
-    }
-}
-
-- (BOOL)isEnabled
-{
-    return self.button.userInteractionEnabled;
-}
-
-#pragma mark - Mutex
-
--(void)lockMutex
-{
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        pthread_mutex_init(&self->_canStartJailbreak, NULL);
-    });
-    pthread_mutex_lock(&self->_canStartJailbreak);
-}
-
--(void)unlockMutex
-{
-    pthread_mutex_unlock(&self->_canStartJailbreak);
-}
+// Các phương thức khác giữ nguyên
 
 @end
